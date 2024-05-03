d07f6760464c8a8c90f0773bb59a20679431ded5
FathGzl <fatihguzel3455@gmail.com>
2024-05-03 09:38:36 +0300

    Cargo Mikroservis => Repository D P ve migration

---
+++ MultShop.Cargo.DataAccessLayer/Abstract/ICargoCustomerDal.cs
@@ -0,0 +1,8 @@
+﻿using MultShop.Cargo.EntityLayer.Concrete;
+
+namespace MultShop.Cargo.DataAccessLayer.Abstract
+{
+    public interface ICargoCustomerDal:IGenericDal<CargoCustomer>
+    {
+    }
+}
