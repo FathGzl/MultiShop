4e57c9d709a0dffdaeaaabd9fb5eca4269cb5973
FathGzl <fatihguzel3455@gmail.com>
2024-05-07 15:44:39 +0300

    basket mikroservis => controller ve konfigurasyon işlemleri

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -16,7 +16,7 @@ namespace MultiShop.IdentityServer
             new ApiResource("ResourceDiscount"){Scopes={ "DiscountFullPermission"}},
             new ApiResource("ResourceOrder"){Scopes={ "OrderFullPermission"}},
             new ApiResource("ResourceCargo"){Scopes={ "CargoFullPermission"}},
-
+            new ApiResource("ResourceBasket"){Scopes={ "BasketFullPermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
 
 
@@ -36,6 +36,7 @@ namespace MultiShop.IdentityServer
             new ApiScope("DiscountFullPermission","Full authority for discount operations"),
             new ApiScope("OrderFullPermission","Full authority for order operations"),
             new ApiScope("CargoFullPermission","Full authority for cargo operations"),
+            new ApiScope("BasketFullPermission","Full authority for basket operations"),
 
             new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
         };
@@ -69,7 +70,7 @@ namespace MultiShop.IdentityServer
                 ClientName="Multi Shop Admin User",
                 AllowedGrantTypes=GrantTypes.ClientCredentials,
                 ClientSecrets={ new Secret("multishopsecret".Sha256())},
-                AllowedScopes={ "DiscountFullPermission", "CatalogFullPermission", "OrderFullPermission","CargoFullPermission",
+                AllowedScopes={ "DiscountFullPermission", "CatalogFullPermission", "OrderFullPermission","CargoFullPermission","BasketFullPermission",
                 IdentityServerConstants.LocalApi.ScopeName,
                 IdentityServerConstants.StandardScopes.Email,
                 IdentityServerConstants.StandardScopes.OpenId,
