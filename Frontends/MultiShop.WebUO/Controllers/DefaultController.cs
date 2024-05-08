72f3e9d2d6225d85385c2cdc22770b70e03f1920
FathGzl <fatihguzel3455@gmail.com>
2024-05-08 10:10:16 +0300

    WebUI => Layout ve Anasayfa tasarım

---
+++ Frontends/MultiShop.WebUO/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUO.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "MultiShop";
+            ViewBag.directory2 = "Ana Sayfa";
+            ViewBag.directory3 = "Ürün Listesi";
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUO/Controllers/UILayout.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUO.Controllers
+{
+    public class UILayout : Controller
+    {
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+    }
+}
