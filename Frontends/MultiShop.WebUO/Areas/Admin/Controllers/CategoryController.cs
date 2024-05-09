03de2c101f6d77274c0d2f1ba0ffc1eda69a51bb
FathGzl <fatihguzel3455@gmail.com>
2024-05-09 15:31:29 +0300

    WebUI Admin => Layout ve Category Ekranı Crud İşlemleri

---
+++ Frontends/MultiShop.WebUO/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Category")]
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            CategoryViewbagList();
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7100/api/categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateCategory")]
+        public IActionResult CreateCategory()
+        {
+            CategoryViewbagList();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateCategory")]
+
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7100/api/categories", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+
+            return View();
+        }
+
+        [Route("DeleteCategory/{id}")]  
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync($"https://localhost:7100/api/categories?id={id}");
+            return RedirectToAction("Index", "Category", new { area = "Admin" });
+        }
+
+        [HttpGet]
+        [Route("UpdateCategory/{id}")]      
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+            CategoryViewbagList();
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7100/api/categories/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateCategory/{id}")]        
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7100/api/categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+
+            return View();            
+        }
+
+
+        void CategoryViewbagList()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0 = "Kategori İşlemleri";
+        }
+    }
+}
